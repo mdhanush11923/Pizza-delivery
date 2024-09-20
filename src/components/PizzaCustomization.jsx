70dec80eff1d0040a862988988cb8f19daf75a49
M Dhanush <mdhanush11923@gmail.com>
2024-09-20 14:11:14 +0530

    modify custom pizza page

--- src/components/PizzaCustomization.jsx
+++ src/components/PizzaCustomization.jsx
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
-import { Select, SelectSection, SelectItem } from "@nextui-org/select";
+import { Select, SelectItem } from "@nextui-org/select";
 import { Button, Checkbox, CheckboxGroup } from '@nextui-org/react';
 import { Image } from '@nextui-org/react';
 import customPizzaImg from "../Images/customPizza.png";
 
+const pizzaBases = [
+  { key: "Thin Crust", label: "Thin Crust" },
+  { key: "Thick Crust", label: "Thick Crust" },
+  { key: "Cheese Burst", label: "Cheese Burst" },
+  { key: "Gluten Free", label: "Gluten Free" },
+  { key: "Whole Wheat", label: "Whole Wheat" }
+];
+
+const sauces = [
+  { key: "Tomato Basil", label: "Tomato Basil" },
+  { key: "Barbeque", label: "Barbeque" },
+  { key: "Pesto", label: "Pesto" },
+  { key: "White Garlic", label: "White Garlic" },
+  { key: "Spicy Buffalo", label: "Spicy Buffalo" }
+];
+
+const cheeses = [
+  { key: "Mozzarella", label: "Mozzarella" },
+  { key: "Cheddar", label: "Cheddar" },
+  { key: "Parmesan", label: "Parmesan" },
+  { key: "Feta", label: "Feta" },
+  { key: "Gouda", label: "Gouda" }
+];
+
+const veggiesOptions = [
+  { key: "Bell Peppers", label: "Bell Peppers" },
+  { key: "Mushrooms", label: "Mushrooms" },
+  { key: "Olives", label: "Olives" },
+  { key: "Onions", label: "Onions" },
+  { key: "Spinach", label: "Spinach" }
+];
+
 const PizzaCustomization = () => {
-  const [base, setBase] = useState('');
-  const [sauce, setSauce] = useState('');
-  const [cheese, setCheese] = useState('');
+  const [base, setBase] = useState(new Set([]));
+  const [sauce, setSauce] = useState(new Set([]));
+  const [cheese, setCheese] = useState(new Set([]));
   const [veggies, setVeggies] = useState([]);
-
-  const handleBaseChange = (event) => {
-    setBase(event.target.value);
-  };
-
-  const handleSauceChange = (event) => {
-    setSauce(event.target.value);
-  };
-
-  const handleCheeseChange = (event) => {
-    setCheese(event.target.value);
-  };
 
   const handleVeggiesChange = (event) => {
     const { value, checked } = event.target;
@@ -32,56 +52,53 @@ const PizzaCustomization = () => {
   };
 
   const handleSubmit = () => {
-    const orderData = { base, sauce, cheese, veggies };
+    const orderData = {
+      base: [...base].join(', '),
+      sauce: [...sauce].join(', '),
+      cheese: [...cheese].join(', '),
+      veggies: veggies.join(', ')
+    };
+    console.log(orderData);
     // Handle submission and payment logic
   };
 
   return (
-    <div className='flex flex-col items-center p-20 pt-0'>
+    <div className='flex flex-col items-center p-20 pt-4'>
       <div className='flex flex-col items-center gap-6'>
         <div className='flex items-center gap-8'>
           <h1 className='scroll-m-20 mb-4 text-4xl font-extrabold tracking-tight lg:text-5xl'>Create Your Own Pizza</h1>
           <Image width={250} isBlurred src={customPizzaImg} />
         </div>
-        {/* Choose Pizza Base */}
-        <Select className='' variant='faded' label="Choose Pizza Base" value={base} onChange={handleBaseChange}>
-          <SelectItem value="Thin Crust">Thin Crust</SelectItem>
-          <SelectItem value="Thick Crust">Thick Crust</SelectItem>
-          <SelectItem value="Cheese Burst">Cheese Burst</SelectItem>
-          <SelectItem value="Gluten Free">Gluten Free</SelectItem>
-          <SelectItem value="Whole Wheat">Whole Wheat</SelectItem>
+
+        <Select variant='faded' label="Choose Pizza Base" selectedKeys={base} onSelectionChange={setBase}>
+          {pizzaBases.map((item) => (
+            <SelectItem key={item.key}>{item.label}</SelectItem>
+          ))}
         </Select>
 
-        {/* Choose Sauce */}
-        <Select variant='faded' label="Choose Sauce" value={sauce} onChange={handleSauceChange}>
-          <SelectItem value="Tomato Basil">Tomato Basil</SelectItem>
-          <SelectItem value="Barbeque">Barbeque</SelectItem>
-          <SelectItem value="Pesto">Pesto</SelectItem>
-          <SelectItem value="White Garlic">White Garlic</SelectItem>
-          <SelectItem value="Spicy Buffalo">Spicy Buffalo</SelectItem>
+        <Select variant='faded' label="Choose Sauce" selectedKeys={sauce} onSelectionChange={setSauce}>
+          {sauces.map((item) => (
+            <SelectItem key={item.key}>{item.label}</SelectItem>
+          ))}
         </Select>
 
-        {/* Choose Cheese */}
-        <Select variant='faded' label="Choose Cheese" value={cheese} onChange={handleCheeseChange}>
-          <SelectItem value="Mozzarella">Mozzarella</SelectItem>
-          <SelectItem value="Cheddar">Cheddar</SelectItem>
-          <SelectItem value="Parmesan">Parmesan</SelectItem>
-          <SelectItem value="Feta">Feta</SelectItem>
-          <SelectItem value="Gouda">Gouda</SelectItem>
+        <Select variant='faded' label="Choose Cheese" selectedKeys={cheese} onSelectionChange={setCheese}>
+          {cheeses.map((item) => (
+            <SelectItem key={item.key}>{item.label}</SelectItem>
+          ))}
         </Select>
 
-        {/* Choose Veggies */}
         <CheckboxGroup color='warning' label="Choose Veggies" orientation="horizontal">
-          <Checkbox value="Bell Peppers" onChange={handleVeggiesChange}> Bell Peppers </Checkbox>
-          <Checkbox value="Mushrooms" onChange={handleVeggiesChange}> Mushrooms </Checkbox>
-          <Checkbox value="Olives" onChange={handleVeggiesChange}> Olives </Checkbox>
-          <Checkbox value="Onions" onChange={handleVeggiesChange}> Onions </Checkbox>
-          <Checkbox value="Spinach" onChange={handleVeggiesChange}> Spinach </Checkbox>
+          {veggiesOptions.map((item) => (
+            <Checkbox key={item.key} value={item.key} onChange={handleVeggiesChange}>
+              {item.label}
+            </Checkbox>
+          ))}
         </CheckboxGroup>
 
-
-        <Button className='max-w-fit' color="success" size='lg' onClick={handleSubmit}>Add to cart</Button>
-        <h1></h1>
+        <Button className='max-w-fit' color="success" size='lg' onClick={handleSubmit}>
+          Add to cart
+        </Button>
       </div>
     </div>
   );
