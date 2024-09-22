bb6f047dcfca1306f255064d7d6c9ff178a5e6e7
M Dhanush <mdhanush11923@gmail.com>
2024-09-23 03:08:33 +0530

    update menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -11,6 +11,8 @@ import { Button } from "@nextui-org/react";
 import PizzaItem from "./PizzaItem";
 import pizzas from "./pizzaData";
 import { vegetarianPizzas, nonVegetarianPizzas } from "./pizzaData";
+import vegIcon from "../Images/veg.png";
+import nonVegIcon from "../Images/nonVeg.png";
 
 export default function Menu() {
   const colors = [
@@ -33,39 +35,42 @@ export default function Menu() {
   const [selectedKey, setSelectedKey] = React.useState("All items");
 
   return (
-    <div className="flex flex-col h-full items-center p-5 md:p-10 gap-6">
-      <div className="flex flex-wrap gap-4 w-full justify-evenly">
+    <div className="flex flex-col h-full items-center p-5 md:p-16 gap-6">
+      <div className="flex flex-wrap gap-4 sm:px-10 w-full justify-center sm:justify-between">
         <h1 className="scroll-m-20 mb-4 font-poppins font-extrabold tracking-tight text-center text-3xl lg:text-4xl">
           {selectedKey}
         </h1>
         <Tabs
-          variant="light"
-          color="success"
           defaultSelectedKey="All items"
           selectedKey={selectedKey}
           onSelectionChange={setSelectedKey}
           classNames={{
-            tab: "h-12 p-3 text-sm font-semibold",
-            tabContent: "w-full group-data-[selected=true]:text-background",
+            tab: "h-12 p-3 font-semibold",
+            tabContent: "w-full group-data-[selected=true]:text-background ",
+            cursor: "bg-foreground",
           }}
-          radius="sm"
+          radius="lg"
+          color="primary"
           aria-label="Tabs sizes"
         >
-          <Tab key="All items" title="All items">
+          <Tab key="All items" title="Both">
             <div className="flex flex-wrap justify-center gap-16">
               {pizzas.map((pizza, index) => (
                 <PizzaItem key={index} id={index} color="bg-peachblossom" />
               ))}
             </div>
           </Tab>
-          <Tab key="Vegetarian" title="Vegetarian">
+          <Tab key="Vegetarian" title={<Image width={30} src={vegIcon} />}>
             <div className="flex flex-wrap  w-full justify-center gap-16">
               {vegetarianPizzas.map((pizza) => (
                 <PizzaItem key={pizza.id} id={pizza.id} color="bg-veggreen" />
               ))}
             </div>
           </Tab>
-          <Tab key="Non Vegetarian" title="Non Vegetarian">
+          <Tab
+            key="Non Vegetarian"
+            title={<Image width={30} src={nonVegIcon} />}
+          >
             <div className="flex flex-wrap justify-center gap-16">
               {nonVegetarianPizzas.map((pizza) => (
                 <PizzaItem key={pizza.id} id={pizza.id} color="bg-[#A1D6B2]" />
