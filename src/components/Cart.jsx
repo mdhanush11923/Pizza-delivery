bb334f80803a51c8a71c3fb228e645ad1b294f60
M Dhanush <mdhanush11923@gmail.com>
2024-09-23 16:45:36 +0530

    update cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,13 @@
-import { Card } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
 import React, { createContext, useContext, useState } from "react";
 
 // Create a Context for the cart
@@ -22,11 +31,69 @@ export const useCart = () => {
 
 export default function Cart() {
   const { cartCount, setCartCount } = useCart();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <div>
       {cartCount > 0 && (
-        <Card isBlurred radius="sm" className="fixed bottom-5 left-10 right-10 bg-green-500 text-white p-4 text-center z-50">
-          {cartCount} {cartCount === 1 ? "item" : "items"} in cart
+        <Card
+          isBlurred
+          isPressable
+          radius="sm"
+          onPress={onOpen}
+          className="fixed bottom-5 left-10 border right-10 p-4 text-center z-50"
+        >
+          <h1>
+            {cartCount} {cartCount === 1 ? "item" : "items"} in cart
+          </h1>
+          <Modal
+            scrollBehavior="inside"
+            backdrop="opaque"
+            isOpen={isOpen}
+            onOpenChange={onOpenChange}
+            classNames={{
+              backdrop: "bg-foreground/50 backdrop-opacity-50",
+            }}
+          >
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1">
+                    Modal Title
+                  </ModalHeader>
+                  <ModalBody>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Nullam pulvinar risus non risus hendrerit venenatis.
+                      Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Nullam pulvinar risus non risus hendrerit venenatis.
+                      Pellentesque sit amet hendrerit risus, sed porttitor quam.
+                    </p>
+                    <p>
+                      Magna exercitation reprehenderit magna aute tempor
+                      cupidatat consequat elit dolor adipisicing. Mollit dolor
+                      eiusmod sunt ex incididunt cillum quis. Velit duis sit
+                      officia eiusmod Lorem aliqua enim laboris do dolor
+                      eiusmod. Et mollit incididunt nisi consectetur esse
+                      laborum eiusmod pariatur proident Lorem eiusmod et. Culpa
+                      deserunt nostrud ad veniam.
+                    </p>
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="danger" variant="light" onPress={onClose}>
+                      Close
+                    </Button>
+                    <Button color="primary" onPress={onClose}>
+                      Action
+                    </Button>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
         </Card>
       )}
     </div>
