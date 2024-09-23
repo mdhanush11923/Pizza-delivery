1e488637b5aa0f604ca74c59dc265f26d0e2598c
M Dhanush <mdhanush11923@gmail.com>
2024-09-23 05:38:46 +0530

    update loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import PizzaCustomization from "./components/PizzaCustomization";
-import Content from "./components/Content";
-import Topbar from "./components/Topbar";
 import { Routes, Route, useNavigate, useHref } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
-import Entry from "./components/Entry";
-import Menu from "./components/Menu";
 import Layout from "./components/Layout";
-import Orders from "./components/Orders";
-import Home from "./components/Home";
 import { CartProvider } from "./components/Cart";
-const LasyMenu = React.lazy(() => import("./components/Menu"));
+
+// Lazy loading all components
+const LazyHome = React.lazy(() => import("./components/Home"));
+const LazyMenu = React.lazy(() => import("./components/Menu"));
+const LazyPizzaCustomization = React.lazy(() => import("./components/PizzaCustomization"));
+const LazyOrders = React.lazy(() => import("./components/Orders"));
+const LazyEntry = React.lazy(() => import("./components/Entry"));
 
 export default function App() {
   const navigate = useNavigate();
+
   const [darkMode, setMode] = React.useState(false);
 
   function changeMode() {
@@ -31,28 +31,57 @@ export default function App() {
               path="/pizza-delivery/"
               element={<Layout darkMode={darkMode} changeMode={changeMode} />}
             >
-              <Route index element={<Home />} />
+              {/* Using Suspense for lazy-loaded routes */}
+              <Route
+                index
+                element={
+                  <React.Suspense fallback={<div>Loading Home...</div>}>
+                    <LazyHome />
+                  </React.Suspense>
+                }
+              />
               <Route
                 path="/pizza-delivery/menu"
                 element={
-                  <React.Suspense fallback="Loading....">
-                    <LasyMenu />
+                  <React.Suspense fallback={<div>Loading Menu...</div>}>
+                    <LazyMenu />
                   </React.Suspense>
                 }
               />
               <Route
                 path="/pizza-delivery/custom"
-                element={<PizzaCustomization />}
+                element={
+                  <React.Suspense
+                    fallback={<div>Loading Customization...</div>}
+                  >
+                    <LazyPizzaCustomization />
+                  </React.Suspense>
+                }
               />
-              <Route path="/pizza-delivery/orders" element={<Orders />} />
+              <Route
+                path="/pizza-delivery/orders"
+                element={
+                  <React.Suspense fallback={<div>Loading Orders...</div>}>
+                    <LazyOrders />
+                  </React.Suspense>
+                }
+              />
             </Route>
             <Route
               path="/pizza-delivery/login"
-              element={<Entry darkMode={darkMode} defaultTab="login" />}
+              element={
+                <React.Suspense fallback={<div>Loading Login...</div>}>
+                  <LazyEntry darkMode={darkMode} defaultTab="login" />
+                </React.Suspense>
+              }
             />
             <Route
               path="/pizza-delivery/signup"
-              element={<Entry darkMode={darkMode} defaultTab="signup" />}
+              element={
+                <React.Suspense fallback={<div>Loading Signup...</div>}>
+                  <LazyEntry darkMode={darkMode} defaultTab="signup" />
+                </React.Suspense>
+              }
             />
           </Routes>
         </CartProvider>
