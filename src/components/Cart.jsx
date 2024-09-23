1226cab3f79e4c80480afd16a4d7447d639424e7
M Dhanush <mdhanush11923@gmail.com>
2024-09-23 15:33:25 +0530

    add cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,3 +1,4 @@
+import { Card } from "@nextui-org/react";
 import React, { createContext, useContext, useState } from "react";
 
 // Create a Context for the cart
@@ -18,3 +19,16 @@ export const CartProvider = ({ children }) => {
 export const useCart = () => {
   return useContext(CartContext);
 };
+
+export default function Cart() {
+  const { cartCount, setCartCount } = useCart();
+  return (
+    <div>
+      {cartCount > 0 && (
+        <Card isBlurred radius="sm" className="fixed bottom-5 left-10 right-10 bg-green-500 text-white p-4 text-center z-50">
+          {cartCount} {cartCount === 1 ? "item" : "items"} in cart
+        </Card>
+      )}
+    </div>
+  );
+}
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Topbar from './Topbar'; // Adjust the import paths as needed
 import { Link } from '@nextui-org/react';
+import Cart from './Cart';
 
 const Layout = ({ darkMode, changeMode }) => (
   <div className="h-full">
     <Topbar condition={darkMode} onSwitch={changeMode} />
     <Outlet />
+    <Cart></Cart>
     <footer className="flex flex-col home-footer w-full h-40 mt-16 gap-2 p-10 sm:h-60 bg-myhouseblue opacity-85 justify-center text-center items-center">
       <p className="text-white">
         &copy; 2024 PIZzA Delivery. All rights reserved.
