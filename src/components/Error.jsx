9805bbc2300bde7b8b1bfe02bac3c34aa45c690b
M Dhanush <mdhanush11923@gmail.com>
2024-09-24 22:57:03 +0530

    update entry

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+  return (
+    <div className={css.outerDiv}>
+      <div className={css.innerDiv}>
+        <h1>Error Page Not Found!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
